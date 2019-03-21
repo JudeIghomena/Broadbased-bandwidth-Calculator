37440fa7b1cc6f49b779949ed072da8e4c63ea43
Jude Oghenekevwe Ighomena <45424348+JudeIghomena@users.noreply.github.com>
2019-03-21 12:38:33 +0100

    Add files via upload

---
+++ precache-manifest.7af29503980db21fe408de0f26bb4634.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9183fa3bb46a98c04154ea236d9944de",
+    "url": "/bbc-bandwidthcal/static/media/email.9183fa3b.svg"
+  },
+  {
+    "revision": "362008ee7871ac83e1f2",
+    "url": "/bbc-bandwidthcal/static/css/main.a39fb141.chunk.css"
+  },
+  {
+    "revision": "5f9a0175f1adc4970fe7",
+    "url": "/bbc-bandwidthcal/static/js/runtime~main.93573c48.js"
+  },
+  {
+    "revision": "67c9e2399c7eec85084dfc2ea045ae44",
+    "url": "/bbc-bandwidthcal/static/media/dpbg.67c9e239.jpg"
+  },
+  {
+    "revision": "41f356c5f93673a621a1",
+    "url": "/bbc-bandwidthcal/static/js/2.5d03b541.chunk.js"
+  },
+  {
+    "revision": "c4e5a06e8cb45ac6a1a498207613b063",
+    "url": "/bbc-bandwidthcal/static/media/logo.c4e5a06e.png"
+  },
+  {
+    "revision": "b2ecda7b7bdfc451c4182013d2f07134",
+    "url": "/bbc-bandwidthcal/static/media/video-conference.b2ecda7b.svg"
+  },
+  {
+    "revision": "5e12d555ebd643d217671a7d8d4b05dd",
+    "url": "/bbc-bandwidthcal/static/media/downloadLarge.5e12d555.svg"
+  },
+  {
+    "revision": "362008ee7871ac83e1f2",
+    "url": "/bbc-bandwidthcal/static/js/main.ea1bf2c6.chunk.js"
+  },
+  {
+    "revision": "fed27edca1271c92c263ea8d36b5c608",
+    "url": "/bbc-bandwidthcal/static/media/media.fed27edc.svg"
+  },
+  {
+    "revision": "5b19238631cd6029ccdb55ab1129b243",
+    "url": "/bbc-bandwidthcal/static/media/largeUpload.5b192386.svg"
+  },
+  {
+    "revision": "59933ce1ec5e133e36b37ba561b0d584",
+    "url": "/bbc-bandwidthcal/static/media/musicStream.59933ce1.svg"
+  },
+  {
+    "revision": "afc1179036851051b0d4a52325baca44",
+    "url": "/bbc-bandwidthcal/static/media/voip.afc11790.svg"
+  },
+  {
+    "revision": "f4b7b889a9b36ae778bf5c7dd872649b",
+    "url": "/bbc-bandwidthcal/static/media/videoStream.f4b7b889.svg"
+  },
+  {
+    "revision": "41f356c5f93673a621a1",
+    "url": "/bbc-bandwidthcal/static/css/2.e0342017.chunk.css"
+  },
+  {
+    "revision": "2a2df3c14feb30b6641460c16709c1be",
+    "url": "/bbc-bandwidthcal/index.html"
+  }
+];
